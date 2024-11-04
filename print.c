321e9f88682ec26c62d0430160237c7d8b9f95c4
kurth4cker <kurth4cker@gmail.com>
2024-11-04 22:36:05 +0300

    add print

--- print.c
+++ print.c
@@ -1,10 +1,3 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 // SPDX-FileCopyrightText: 2024 kurth4cker <kurth4cker@gmail.com>
 
-#include <stdio.h>
-
-int
-main(void)
-{
-	printf("hello world\n");
-}
---
+++ print.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <array.h>
+
+void print_farray(const farray *);
